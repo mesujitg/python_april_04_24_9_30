98f5a2f3adcab60ed32caf983e7db33d746e3dc5
mesujitg <mesujitg@gmail.com>
2022-05-18 11:49:03 +0545

    oop text file

---
+++ oop.py
@@ -0,0 +1,54 @@
+# class & object
+'''
+class Student
+{
+    private string name;
+    private string roll_no;
+
+
+    public void register(){}
+
+    public void login(){}
+}
+
+class Dog:
+    height = ''
+    breed = ''
+    color = ''
+
+    def makesound():
+        pass
+    
+    def moves():
+        pass
+
+
+'''
+class Student:
+    name = ''
+    roll_no = ''
+    phone = ''
+    email = ''
+    address = ''
+    dob = ''
+    gender = ''
+    course = ''
+    semester = 1
+
+    # CRUD
+
+    def addStudent(): 
+        pass
+
+    def selectStudent():
+        pass
+
+    def updateStudent():
+        pass
+
+    def leave():
+        pass
+
+    def pay():
+        pass
+
