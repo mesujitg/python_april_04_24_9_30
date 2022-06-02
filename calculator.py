c87a6a521081703d449fd5a2ec8b59092d741e69
mesujitg <mesujitg@gmail.com>
2022-06-02 11:26:24 +0545

    calculator

--- calculator.py
+++ calculator.py
@@ -1,95 +1,115 @@
 from tkinter import *
 
-num1 = 0
-num2 = 0
-op = ''
 
-def btn_click(val):
-    global num1, num2, op
-    if val == 'c':
-        display.delete(0, END)
-    elif val == '+' or val == '-' or val == '*' or val == '/':
-        num1 = float(display.get())
-        op = val
-        display.delete(0, END)
-    elif val == '=':
-        num2 = float(display.get())
-        if op == '+':
-            result = num1 + num2
-        elif op == '-':
-            result = num1 - num2
-        elif op == '*':
-            result = num1 * num2
-        elif op == '/':
-            result = num1 / num2
-        
-        display.delete(0, END)
-        display.insert(END, result)
-    else:
-        display.insert(END, val)
+class Calculator:
+    def __init__(self) -> None:
+        self.num1 = 0
+        self.num2 = 0
+        self.op = ''
+
+        self.root = Tk()
+        self.root.title('Calculator')
+
+        self.display = Entry(self.root, font=('Arial', 24), width=12, justify='right')
+        self.display.grid(row=0, column=0, columnspan=4, padx=20, pady=20)
+
+        # first row
+        self.btn7 = Button(self.root, text="7", height=3, width=6, command=lambda: self.btn_click(7))
+        self.btn7.grid(row=1, column=0)
+
+        self.btn8 = Button(self.root, text="8", height=3, width=6, command=lambda: self.btn_click(8))
+        self.btn8.grid(row=1, column=1)
+
+        self.btn9 = Button(self.root, text="9", height=3, width=6, command=lambda: self.btn_click(9))
+        self.btn9.grid(row=1, column=2)
+
+        self.btn_div = Button(self.root, text="/", height=3, width=6, command=lambda: self.btn_click('/'))
+        self.btn_div.grid(row=1, column=3)
 
 
-root = Tk()
-root.title('Calculator')
+        # second row
+        self.btn4 = Button(self.root, text="4", height=3, width=6, command=lambda: self.btn_click(4))
+        self.btn4.grid(row=2, column=0)
 
-display = Entry(root, font=('Arial', 24), width=12)
-display.grid(row=0, column=0, columnspan=4, padx=20, pady=20)
+        self.btn5 = Button(self.root, text="5", height=3, width=6, command=lambda: self.btn_click(5))
+        self.btn5.grid(row=2, column=1)
 
-# first row
-btn7 = Button(root, text="7", height=3, width=6, command=lambda: btn_click(7))
-btn7.grid(row=1, column=0)
+        self.btn6 = Button(self.root, text="6", height=3, width=6, command=lambda: self.btn_click(6))
+        self.btn6.grid(row=2, column=2)
 
-btn8 = Button(root, text="8", height=3, width=6, command=lambda: btn_click(8))
-btn8.grid(row=1, column=1)
-
-btn9 = Button(root, text="9", height=3, width=6, command=lambda: btn_click(9))
-btn9.grid(row=1, column=2)
-
-btn_div = Button(root, text="/", height=3, width=6, command=lambda: btn_click('/'))
-btn_div.grid(row=1, column=3)
+        self.btn_mul = Button(self.root, text="x", height=3, width=6, command=lambda: self.btn_click('*'))
+        self.btn_mul.grid(row=2, column=3)
 
 
-# second row
-btn4 = Button(root, text="4", height=3, width=6, command=lambda: btn_click(4))
-btn4.grid(row=2, column=0)
+        # third row
+        self.btn1 = Button(self.root, text="1", height=3, width=6, command=lambda: self.btn_click(1))
+        self.btn1.grid(row=3, column=0)
 
-btn5 = Button(root, text="5", height=3, width=6, command=lambda: btn_click(5))
-btn5.grid(row=2, column=1)
+        self.btn2 = Button(self.root, text="2", height=3, width=6, command=lambda: self.btn_click(2))
+        self.btn2.grid(row=3, column=1)
 
-btn6 = Button(root, text="6", height=3, width=6, command=lambda: btn_click(6))
-btn6.grid(row=2, column=2)
+        self.btn3 = Button(self.root, text="3", height=3, width=6, command=lambda: self.btn_click(3))
+        self.btn3.grid(row=3, column=2)
 
-btn_mul = Button(root, text="x", height=3, width=6, command=lambda: btn_click('*'))
-btn_mul.grid(row=2, column=3)
+        self.btn_sub = Button(self.root, text="-", height=3, width=6, command=lambda: self.btn_click('-'))
+        self.btn_sub.grid(row=3, column=3)
+
+        # fourth row
+        self.btnc = Button(self.root, text="C", height=3, width=6, command=lambda: self.btn_click('c'))
+        self.btnc.grid(row=4, column=0)
+
+        self.btn0 = Button(self.root, text="0", height=3, width=6, command=lambda: self.btn_click(0))
+        self.btn0.grid(row=4, column=1)
+
+        self.btn_eq = Button(self.root, text="=", height=3, width=6, command=lambda: self.btn_click('='))
+        self.btn_eq.grid(row=4, column=2)
+
+        self.btn_add = Button(self.root, text="+", height=3, width=6, command=lambda: self.btn_click('+'))
+        self.btn_add.grid(row=4, column=3)
+
+        # . backspace +/- % 
+        self.btnsign = Button(self.root, text="+/-", height=3, width=6, command=lambda: self.btn_click('+/-'))
+        self.btnsign.grid(row=5, column=0)
+
+        self.btndot = Button(self.root, text=".", height=3, width=6, command=lambda: self.btn_click('.'))
+        self.btndot.grid(row=5, column=1)
+
+        self.btn_bs = Button(self.root, text="x <=", height=3, width=6, command=lambda: self.btn_click('bs'))
+        self.btn_bs.grid(row=5, column=2)
+
+        self.btn_p = Button(self.root, text="%", height=3, width=6, command=lambda: self.btn_click('%'))
+        self.btn_p.grid(row=5, column=3)
+
+        self.root.mainloop()
+
+    def btn_click(self, val):
+        if val == 'c':
+            self.display.delete(0, END)
+        elif val == '+' or val == '-' or val == '*' or val == '/':
+            self.num1 = float(self.display.get())
+            self.op = val
+            self.display.delete(0, END)
+        elif val == '=':
+            self.num2 = float(self.display.get())
+            if self.op == '+':
+                result = self.num1 + self.num2
+            elif self.op == '-':
+                result = self.num1 - self.num2
+            elif self.op == '*':
+                result = self.num1 * self.num2
+            elif self.op == '/':
+                result = self.num1 / self.num2
+            
+            self.display.delete(0, END)
+            self.display.insert(END, result)
+        elif val == '+/-':
+            num = self.display.get()
+            if num[0] == '-':
+                self.display.delete(0)
+            else:
+                self.display.insert(0, '-')
+        else:
+            self.display.insert(END, val)
 
 
-# third row
-btn1 = Button(root, text="1", height=3, width=6, command=lambda: btn_click(1))
-btn1.grid(row=3, column=0)
-
-btn2 = Button(root, text="2", height=3, width=6, command=lambda: btn_click(2))
-btn2.grid(row=3, column=1)
-
-btn3 = Button(root, text="3", height=3, width=6, command=lambda: btn_click(3))
-btn3.grid(row=3, column=2)
-
-btn_sub = Button(root, text="-", height=3, width=6, command=lambda: btn_click('-'))
-btn_sub.grid(row=3, column=3)
-
-# fourth row
-btnc = Button(root, text="C", height=3, width=6, command=lambda: btn_click('c'))
-btnc.grid(row=4, column=0)
-
-btn0 = Button(root, text="0", height=3, width=6, command=lambda: btn_click(0))
-btn0.grid(row=4, column=1)
-
-btn_eq = Button(root, text="=", height=3, width=6, command=lambda: btn_click('='))
-btn_eq.grid(row=4, column=2)
-
-btn_add = Button(root, text="+", height=3, width=6, command=lambda: btn_click('+'))
-btn_add.grid(row=4, column=3)
-
-
-# . backspace +/- % 
-
-root.mainloop()
+Calculator()
